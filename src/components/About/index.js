1e2ae1e7a1e8f27306ca2124d90cfcadb70c0d24
yulduz <yulduz83@gmail.com>
2021-04-24 11:53:53 -0500

    adds PhotoList

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,12 +3,6 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 
 // React components must always return a single parent JSX element. However, this element may have many children elements.
 const About = () => {
-  //   return (
-  //     <section>
-  //       <h1 id="about">Who am I?</h1>
-  //     </section>
-  //   );
-
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
